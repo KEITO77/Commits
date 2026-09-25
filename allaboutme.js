73edb8a0ff5e579a605c18c8badfaa5e3801e7a7
KEITO077 <vxx0iiikate@gmail.com>
2026-09-25 21:47:46 +0800

    11th commit

--- allaboutme.js
+++ allaboutme.js
@@ -1,4 +1,6 @@
 let Hobbies = ("Reading Books, solving rubik's cube, hiking, traveling, playing archery, outdoor activities");
 let books = ("the 5 university series of  Gwy Saludes , A Gentle Reminder, The Let Them Theory, The Art of War, and more.");
+let hikelist = ("Malopalo Cave and Marble park, Mt. Lupao, Mt. Nakilingan, Brgy. Pena Tower, Angel Falls, Busay Falls, and more.");
 console.log("My hobbies are", Hobbies);
 console.log("The book that I already read is", books);
+console.log("The places that I already hiked are", hikelist);
