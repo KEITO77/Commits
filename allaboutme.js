69e0334386c87d2bd0446c5d9a29069bc4d95ed9
KEITO077 <vxx0iiikate@gmail.com>
2026-09-25 21:00:31 +0800

    all about me

---
+++ allaboutme.js
@@ -0,0 +1,2 @@
+let Hobbies = ["Reading Books", "solving rubik's cube", "hiking", "traveling","playing archery", "outdoor activities"];
+console.log("My hobbies are", Hobbies[0]+",", Hobbies[1]+",", Hobbies[2]+",", Hobbies[3]+",", Hobbies[4]+", and other", Hobbies[5]);
