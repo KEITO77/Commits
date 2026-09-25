eca20fb632ecced6c168507ddb23f8f01de529ac
KEITO077 <vxx0iiikate@gmail.com>
2026-09-25 20:45:39 +0800

    8th commit

--- hello.js
+++ hello.js
@@ -4,4 +4,4 @@ let age = 23;
 let address = "Brgy. San Joaquin, Calbayog City";
 let school = "Northwest Samar State University";
 let course = "BSIT";
-console.log("My name is", name, surname, "and I am", age, "years old.", "I live in", address, "and I study at", school);
+console.log("My name is", name, surname, "and I am", age, "years old.", "I live in", address, "and I study at", school, "taking up", course);
