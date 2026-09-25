4f9ba28c83380dcef0e8cf21744a8f281c39af17
KEITO077 <vxx0iiikate@gmail.com>
2026-09-25 20:40:30 +0800

    6th commit

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 let name = "Kate";
-let surname ="Comonical";
+let surname = "Comonical";
 let age = 23;
 let address = "Brgy. San Joaquin, Calbayog City";
-console.log("My name is", name, surname, "and I am", age, "years old.", "I live in", address);
+let school = "Northwest Samar State University";
+console.log("My name is", name, surname, "and I am", age, "years old.", "I live in", address, "and I study at", school);
